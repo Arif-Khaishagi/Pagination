cc692d3ce3eb8c04ed3e8e5ff02fe7e5e7676ad3
Asif <asif.khan.khargone@gmail.com>
2024-09-12 23:40:33 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Table from './Table';
+// import [useEffect, useState] 
+
+function App() {
+  const [employees, setEmployees] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(employees.length / 10);
+
+  let pages = [];
+
+  for (let i = 1; i <= totalPages; i++) {
+    pages.push(i);
+  }
+
+  const fetchData = async () => {
+    try {
+      await fetch(
+        "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+      )
+        .then((res) => res.json())
+        .then((data) => setEmployees(data));
+    } catch (error) {
+      alert("failed to fetch data");
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handlePrevious = () => {
+    setCurrentPage((prev) => prev - 1);
+  };
+
+  const handleNext = () => {
+    setCurrentPage((prev) => prev + 1);
+  };
+
+  const handleClick = (page) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <>
+      <h1>Employee Data Table</h1>
+      <Table data={employees} currentPage={currentPage} />
+      <div className="pagination">
+        <button
+          type="button"
+          onClick={handlePrevious}
+          disabled={currentPage <= 1}
+        >
+          Previous
+        </button>
+
+        {pages.map((page) => (
+          <button
+            className={currentPage === page ? "activeBtn" : ""}
+            type="button"
+            key={page}
+            onClick={() => handleClick(page)}
+          >
+            {page}
+          </button>
+        ))}
+        <button
+          type="button"
+          onClick={handleNext}
+          disabled={currentPage >= totalPages}
+        >
+          Next
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
